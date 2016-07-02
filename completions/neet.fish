3acc8a9141063e15b366ab0238676557262b74d4
Camille Scholtz <onodera@openmailbox.org>
2016-07-02 22:53:11 +0200

    completions: Follow style

--- completions/neet.fish
+++ completions/neet.fish
@@ -1,4 +1,4 @@
-complete -f -c neet -o e -l edit -d 'Edit list'
-complete -f -c neet -o d -l debug -d 'Print some helpful debug output'
-complete -f -c neet -o a -l alias -d 'forgets current alias'
-complete -f -c neet -o h -l help -d 'Print help and exit'
+complete -c neet -f -o e -l edit -d 'Edit list'
+complete -c neet -f -o d -l debug -d 'Print some helpful debug output'
+complete -c neet -f -o a -l alias -d 'forgets current alias'
+complete -c neet -f -o h -l help -d 'Print help and exit'
